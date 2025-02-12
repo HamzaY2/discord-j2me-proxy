fc62ecdff8dca0b2d0043ff40afc113b354fc41d
HamzaYslmn <hamzayesilmen7@gmail.com>
2025-02-12 16:35:11 +0300

    Refactor start script to separate service startup functions and improve logging

--- start.sh
+++ start.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set -e
 
-run() {
-  local workdir="$1"
-  local message="$2"
-  shift 2
-  echo "$message"
-  if [ -n "$workdir" ]; then
-    (cd "$workdir" && exec "$@")
-  else
-    exec "$@"
-  fi
+start_proxy() {
+  echo "Starting frontend..."
+  cd /app/proxy
+  node .
 }
 
-# Start each service in the background.
-run "/app/proxy" "Starting proxy..." node . &
-run "/app/websocket" "Starting websocket..." node out &
-run "" "Starting nginx..." nginx -g "daemon off;" &
+start_websocket() {
+  echo "Starting frontend..."
+  cd /app/websocket
+  node out
+}
 
+start_nginx() {
+  echo "Starting nginx..."
+  nginx -g "daemon off;"
+}
+
+start_proxy &
+start_websocket &
+start_nginx &
 wait
