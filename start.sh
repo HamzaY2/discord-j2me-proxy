c43b873800643545f00d8795cfd65a8fd43c821a
HamzaYslmn <hamzayesilmen7@gmail.com>
2025-02-12 16:42:41 +0300

    Simplify start script by removing separate function definitions for service startup

--- start.sh
+++ start.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 set -e
 
-start_proxy() {
-  echo "Starting frontend..."
-  cd /app/proxy
-  node .
-}
+echo "Starting frontend..."
+cd /app/proxy
+node .
 
-start_websocket() {
-  echo "Starting frontend..."
-  cd /app/websocket
-  node out
-}
+echo "Starting frontend..."
+cd /app/websocket
+node out
 
-start_nginx() {
-  echo "Starting nginx..."
-  nginx -g "daemon off;"
-}
+echo "Starting nginx..."
+nginx -g "daemon off;"
 
-start_proxy &
-start_websocket &
-start_nginx &
 wait
